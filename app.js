181898cd6fc9a46e7ca288ba0d9eb5c755210fef
Rui Tang <trtdytr93629@gmail.com>
2020-11-19 16:37:02 -0600

    Part2-Using middlewawre

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.send(err.message);
+});
 
 app.listen(3000, () => console.log('Listening on port: 3000......'));
