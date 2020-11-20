d829190e648886a2259d7259b9f02321334ace0c
Rui Tang <trtdytr93629@gmail.com>
2020-11-19 22:18:10 -0600

    Part4-Express view engine

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 
-app.get('/', (req, res) => {
-    // find index.html using absolue path
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.render('index'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/signup', (req, res) => res.render('signup'));
+app.get('/profile', (req, res) => res.render('profile'));
+app.get('/profile/edit', (req, res) => res.render('editProfile'));
 
 app.use((req, res, next) => {
     const err = new Error('Page Not Found');
