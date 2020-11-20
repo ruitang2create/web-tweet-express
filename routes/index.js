6ec08793b842268118b5704276565f4dd12ca8f4
Rui Tang <trtdytr93629@gmail.com>
2020-11-20 16:39:53 -0600

    changed tweets data source from static json file to mongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const tweets = require('../tweets');
-router.get('/', (req, res) => res.render('index', { tweets }));
+// const tweets = require('../tweets');
+// router.get('/', (req, res) => res.render('index', { tweets }));
 
-// const Tweets = require('../models/tweets');
-// router.get('/', (req, res) => {
-//     Tweets.find({}, (err, tweets) => {
-//         res.render('index', { tweets });
-//     });
-// });
+const Tweets = require('../models/tweets');
+router.get('/', (req, res) => {
+    Tweets.find({}, (err, tweets) => {
+        res.render('index', { tweets });
+    });
+});
 router.get('/login', (req, res) => res.render('login'));
 router.get('/signup', (req, res) => res.render('signup'));
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const tweets = require('../tweets');
+// const tweets = require('../tweets');
 
-router.get('/', (req, res) => res.render('profile', { tweets }));
-router.get('/edit', (req, res) => res.render('editProfile', { tweets }));
+// router.get('/', (req, res) => res.render('profile', { tweets }));
+const Tweets = require('../models/tweets');
+router.get('/', (req, res) => {
+    Tweets.find({}, (err, tweets) => {
+        res.render('profile', { tweets });
+    });
+});
+
+router.get('/edit', (req, res) => {
+    Tweets.find({}, (err, tweets) => {
+        res.render('editProfile', { tweets });
+    });
+});
 
 module.exports = router;
